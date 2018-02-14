236c3046a322baaa1550ada8b5125682972beca4
Michail Xirouchakis <michail.xirouchakis@gmail.com>
2018-02-14 11:35:22 +0100

    Added interaction with InfluxDB which runs on localhost

--- BUCK
+++ BUCK
@@ -1,6 +1,26 @@
 COMPILE_DEPS = [
     '//lib:CORE_DEPS',
     '//lib:JACKSON',
+    '//lib:METRICS',
+    '//lib:org.apache.karaf.shell.console',
+    '//lib:metrics-influxdb',
+    '//lib:influxdb-java',
+    '//lib:commons-codec',
+    '//lib:retrofit',
+    '//lib:okhttp',
+    '//lib:okio',
+    '//lib:gson',
+    '//cli:onos-cli',
+]
+
+EXCLUDED_BUNDLES = [
+    '//lib:metrics-influxdb',
+    '//lib:influxdb-java',
+    '//lib:commons-codec',
+    '//lib:retrofit',
+    '//lib:okhttp',
+    '//lib:gson',
+    '//lib:okio',
 ]
 
 osgi_jar (
@@ -13,4 +33,5 @@ onos_app (
     url = 'http://onosproject.org',
     description = 'Port Statistics for device monitoring.',
     required_apps = [ ],
+    excluded_bundles = EXCLUDED_BUNDLES,
 )
--- src/main/java/org/onosproject/portstatistics/Monitor.java
+++ src/main/java/org/onosproject/portstatistics/Monitor.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.onosproject.port.statistics;
+package org.onosproject.portstatistics;
 
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
@@ -34,12 +34,23 @@ import java.util.Iterator;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDB.ConsistencyLevel;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Query;
+import org.influxdb.dto.QueryResult;
+import org.influxdb.dto.QueryResult.Series;
+import org.influxdb.dto.BatchPoints;
+import org.influxdb.dto.Point;
 
 @Component(immediate = true)
 public class Monitor {
 
-    private static final String APP_NAME = "org.onosproject.port.statistics";
+    private static final String APP_NAME = "org.onosproject.portstatistics";
     private static final String PRINT_LEGEND = "[Port Statistics] ";  // Used by Logger
+    private static final String DATABASE_NAME = "port_statistics";
+    private static final String RETENTION_POLICY_NAME = "last_hour";
+    private InfluxDB influxDB;
 
     private final Logger log = getLogger(getClass());
 	private ApplicationId appId;  // Do I need this?
@@ -52,6 +63,8 @@ public class Monitor {
 
     @Activate
     public void activate() {
+        connectToInfluxDB();
+
         appId = coreService.registerApplication(APP_NAME);  // Do I need this?
         log.info(PRINT_LEGEND + "- App Started");
 
@@ -68,6 +81,7 @@ public class Monitor {
 
     @Deactivate
     public void deactivate() {
+        queryInfluxDB();
         log.info(PRINT_LEGEND + "- App Stopped");
     }
 
@@ -82,20 +96,54 @@ public class Monitor {
             for(Port port : ports)
             {
                 PortStatistics portstat = deviceService.getStatisticsForPort(d.id(), port.number());
-                PortStatistics portdeltastat = deviceService.getDeltaStatisticsForPort(d.id(), port.number());
                 
-                if(portstat != null)
-                    log.info(PRINT_LEGEND + "Port " + port.number() + ">> portstat bytes recieved: " + portstat.bytesReceived());
+                if(portstat != null) {
+                    log.info(PRINT_LEGEND + "Port " + port.number() + ">> Bytes, Rx: " + portstat.bytesReceived() + ", Tx: " + portstat.bytesSent());
+                    insertIntoInfluxDB(d.id().toString(), port.number().toString(), portstat.bytesReceived(), portstat.bytesSent());
+                }
                 else
                     log.info(PRINT_LEGEND + "Port " + port.number() + ">> unable to read portStats!");
 
-                if(portdeltastat != null)
-                    log.info(PRINT_LEGEND + "Port " + port.number() + ">> portdeltastat bytes recieved: " + portdeltastat.bytesReceived());
-                else
-                    log.info(PRINT_LEGEND + "Port " + port.number() + ">> Unable to read portDeltaStats!");
             }
         }
     }
 
+    private void connectToInfluxDB() {
+        log.info(PRINT_LEGEND + "########## Connecting to InfluxDB ##########");
+        influxDB = InfluxDBFactory.connect("http://127.0.0.1:8086", "root", "root");  // user credentials silently ignored
+        log.info(PRINT_LEGEND + "########## Connection to InfluxDB successful ##########");
+    }
+
+    private List<QueryResult.Result> queryInfluxDB() {
+        Query query = new Query("SELECT * FROM port_counters", DATABASE_NAME);
+        List<QueryResult.Result> results = influxDB.query(query).getResults();
+
+        if (results != null && results.get(0) != null && results.get(0).getSeries() != null) {
+            for (Series result: results.get(0).getSeries()) log.info(PRINT_LEGEND + " ########## " + result.toString()); 
+        }
+
+
+        return results;
+    }
+
+    private void insertIntoInfluxDB(String device_id, String port_id, long rx_bytes, long tx_bytes) {
+        // InfluxDB tags: device_id, port_id
+        // InfluxDB fields: rx_bytes, tx_bytes
+        BatchPoints batchPoints = BatchPoints
+            .database(DATABASE_NAME)
+            .tag("device_id", device_id)
+            .tag("port_id", port_id)
+            .retentionPolicy(RETENTION_POLICY_NAME)
+            .consistency(ConsistencyLevel.ALL)
+            .build();
+        Point point1 = Point.measurement("port_counters")
+            .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+            .addField("rx_bytes", rx_bytes)
+            .addField("tx_bytes", tx_bytes)
+            .build();
+
+        batchPoints.point(point1);
+        influxDB.write(batchPoints);
+        }
 }
 
