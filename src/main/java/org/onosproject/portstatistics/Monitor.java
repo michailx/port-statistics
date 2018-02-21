f531b3dfd2f758402b239fc3a15ecfbf31f43655
Michail Xirouchakis <michail.xirouchakis@gmail.com>
2018-02-21 15:26:50 +0100

    Draft -- Using Network Configuration Subsystem to add config to my app.

--- src/main/java/org/onosproject/portstatistics/Monitor.java
+++ src/main/java/org/onosproject/portstatistics/Monitor.java
@@ -34,6 +34,7 @@ import java.util.Iterator;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.*;
 import org.influxdb.InfluxDB;
 import org.influxdb.InfluxDB.ConsistencyLevel;
 import org.influxdb.InfluxDBFactory;
@@ -42,6 +43,16 @@ import org.influxdb.dto.QueryResult;
 import org.influxdb.dto.QueryResult.Series;
 import org.influxdb.dto.BatchPoints;
 import org.influxdb.dto.Point;
+import org.onosproject.net.config.NetworkConfigListener;
+import org.onosproject.net.config.NetworkConfigEvent;
+import org.onosproject.net.config.NetworkConfigEvent.Type;
+import org.onosproject.net.config.NetworkConfigRegistry;
+import org.onosproject.net.config.ConfigFactory;
+import org.onosproject.net.config.basics.SubjectFactories;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import org.onosproject.core.ApplicationId;
+import org.onosproject.net.config.Config;
 
 @Component(immediate = true)
 public class Monitor {
@@ -53,19 +64,37 @@ public class Monitor {
     private InfluxDB influxDB;
 
     private final Logger log = getLogger(getClass());
-	private ApplicationId appId;  // Do I need this?
+	private ApplicationId appId;
 
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
     protected DeviceService deviceService;
 
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected NetworkConfigRegistry netcfgRegistry;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
     protected CoreService coreService;
+
+    private final InternalNetworkConfigListener netcfgListener = new InternalNetworkConfigListener();
+
+    private final ConfigFactory<ApplicationId, MonitorConfig> monitorConfigFactory =
+            new ConfigFactory<ApplicationId, MonitorConfig>(
+                    SubjectFactories.APP_SUBJECT_FACTORY,
+                    MonitorConfig.class, "prediction") {
+                @Override
+                public MonitorConfig createConfig() {
+                    return new MonitorConfig();
+                }
+            };
 
     @Activate
     public void activate() {
+        netcfgRegistry.addListener(netcfgListener);
+        netcfgRegistry.registerConfigFactory(monitorConfigFactory);
+
         connectToInfluxDB();
 
-        appId = coreService.registerApplication(APP_NAME);  // Do I need this?
+        appId = coreService.registerApplication(APP_NAME);
         log.info(PRINT_LEGEND + "- App Started");
 
         // Schedule
@@ -83,6 +112,9 @@ public class Monitor {
     public void deactivate() {
         queryInfluxDB();
         log.info(PRINT_LEGEND + "- App Stopped");
+
+        netcfgRegistry.removeListener(netcfgListener);
+        netcfgRegistry.unregisterConfigFactory(monitorConfigFactory);
     }
 
     private void getMonitorData() {
@@ -111,6 +143,7 @@ public class Monitor {
     private void connectToInfluxDB() {
         log.info(PRINT_LEGEND + "########## Connecting to InfluxDB ##########");
         influxDB = InfluxDBFactory.connect("http://127.0.0.1:8086", "root", "root");  // user credentials silently ignored
+        // influxDB.setDatabase(DATABASE_NAME);
         log.info(PRINT_LEGEND + "########## Connection to InfluxDB successful ##########");
     }
 
@@ -144,6 +177,42 @@ public class Monitor {
 
         batchPoints.point(point1);
         influxDB.write(batchPoints);
+    }
+
+    private class InternalNetworkConfigListener implements NetworkConfigListener {
+        @Override
+        public void event(NetworkConfigEvent event) {
+            log.info("NetworkConfigListener " + event.configClass().getClass().getName());
+            log.info("Event Type: " + event.type());
+            if (event.type() == NetworkConfigEvent.Type.CONFIG_ADDED) {
+                Iterator<Map.Entry<String, JsonNode>> ingressPoints = ((MonitorConfig) event.config().get()).getIngressPoints();
+                while (ingressPoints.hasNext()) {
+                	log.info("LOL");
+			    	Map.Entry<String, JsonNode> ingPoint = ingressPoints.next();
+    				log.info(ingPoint.getKey() + ":" + ingPoint.getValue());
+ 				}
+            }
         }
+    }
+
+    public class MonitorConfig extends Config<ApplicationId> {
+
+    	public Iterator<Map.Entry<String, JsonNode>> getIngressPoints(){
+        	ArrayNode parent = (ArrayNode) object.path("ingressPoints");
+        	if (parent.isMissingNode()) {
+            	return null;
+        	} else {
+        		log.info("To String:" + parent.toString() + " -- " + parent.getNodeType().toString());
+        		Iterator<JsonNode> children = parent.elements();
+        		while (children.hasNext()){
+        			JsonNode childNode = children.next();
+					log.info(childNode.get("ingressSwitch").asText() + " -- " + childNode.get("ingressPort").asInt());
+        		}
+
+        		return parent.fields();
+        	}
+    	}
+    }
+
 }
 
